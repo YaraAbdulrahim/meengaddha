174e8acbbcf5bc68525b70db13a58e5f5276ddb0
YaraAbdulrahim <yaraw11w@gmail.com>
2025-09-24 15:05:46 +0300

    header commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
-
+import react  from 'react'
+import Header from './Component/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-            
+      <div>
+        <Header/>
+        </div>      
     </>
   )
 }
