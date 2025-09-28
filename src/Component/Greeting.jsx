3ecf2588871a94a7471d25b448a11e48c1a20751
YaraAbdulrahim <yaraw11w@gmail.com>
2025-09-28 10:06:15 +0300

    Update Greeting Section

--- src/Component/Greeting.jsx
+++ src/Component/Greeting.jsx
@@ -9,7 +9,7 @@ return(
 <p className="font-medium text-2xl mt-8 text-red-800">أسئلة سريعة , منافسة حامية , و متعة سعودية ما تنسى </p>
 
 <div>
-    <button className="border-1 rounded-md mt-12 bg-red-500  text-white px-6 py-3 text-sm">ابدأ اللعب !</button>
+    <button className="border-1 rounded-md mt-12 bg-red-500  text-white px-6 py-3 text-sm">ابدأ اللعب</button>
 </div>
 
 </div>
