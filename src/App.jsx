754044220188d1b53ddbc7df52ba3039569202e9
Arwaf8 <Arwafahad86@gmail.com>
2025-10-12 10:45:47 +0300

    add new routes to my new pages (profile and changePassword)

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,25 @@ import GameDescription from "./components/GameDescription.jsx";
 import Signup from "./components/Signup.jsx";
 import Signin from "./components/Signin.jsx";
 import CategoriesPopup from "./components/CategoriesPopup.jsx";
-import { Routes, Route, Navigate } from "react-router-dom";
-import { BrowserRouter, Routes, Route, Navigate,  } from 'react-router-dom';
-import Homepage from './components/Homepage.jsx'
-
-
-function App() {
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Homepage from "./components/Homepage.jsx";
+import Profile from "./components/Profile.jsx";
+import ChangePassword from "./components/ChangePassword.jsx";
 
 
 export default function App() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center">
+      <div className="min-h-screen">
+
         <Routes>
-          {/* الصفحة الرئيسية */}
+          {/*  Main page: */}
           <Route
             path="/"
             element={
               <>
+               <div className="flex flex-col items-center justify-center"></div>
+
                 <img src="/img.png" alt="" className="mx-auto my-4 max-h-32" />
                 <Header />
                 <Greeting />
@@ -31,9 +32,11 @@ export default function App() {
             }
           />
 
-          {/* Auth */}
+          {/* Rest of the pages */}
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Signin />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile/change-password" element={<ChangePassword />} />
 
           <Route
             path="/categories"
@@ -48,7 +51,6 @@ export default function App() {
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </div>
-
     </>
   );
 }
